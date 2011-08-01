6768319f16f041a3bf7139804d6106a186aeb49f
Mark Johnson <markjohnson@gmail.com>
2011-08-01 10:55:51 -0700

    Add comment about videoHolder.

--- Shelby/NavigationViewController.h
+++ Shelby/NavigationViewController.h
@@ -15,7 +15,7 @@
     IBOutlet UIView *header;
     IBOutlet UIView *videoTableHolder; // this view just tells us where in device .xib file to show the video table
     VideoTableViewController *videoTable;
-    IBOutlet UIView *videoHolder;
+    IBOutlet UIView *videoHolder; // main navigation view for iPhone, view off to the side for iPad
 
 }
 
