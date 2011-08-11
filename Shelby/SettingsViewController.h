bfbf2c9010a5d11b6d334dbeded23a7ac4380b28
David Young-Chan Kay <davidykay@gmail.com>
2011-08-11 11:24:19 -0400

    Added settings controller. haven't done anything with it yet.

---
+++ Shelby/SettingsViewController.h
@@ -0,0 +1,16 @@
+//
+//  SettingsViewController.h
+//  Shelby
+//
+//  Created by David Young-Chan Kay on 8/11/11.
+//  Copyright 2011 Gargoyle Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface SettingsViewController : UIViewController {
+    
+}
+
+@end
