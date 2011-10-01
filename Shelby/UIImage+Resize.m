8884987a7555a4adaededdbcf0ed53fc15ae66f2
Mark Johnson <markjohnson@gmail.com>
2011-09-30 18:55:27 -0700

    Shut up compiler warnings.

--- Shelby/UIImage+Resize.m
+++ Shelby/UIImage+Resize.m
@@ -166,6 +166,10 @@
             transform = CGAffineTransformTranslate(transform, 0, newSize.height);
             transform = CGAffineTransformRotate(transform, -M_PI_2);
             break;
+            
+        case UIImageOrientationUp:
+        case UIImageOrientationUpMirrored:
+            break;
     }
     
     switch (self.imageOrientation) {
@@ -179,6 +183,12 @@
         case UIImageOrientationRightMirrored:  // EXIF = 7
             transform = CGAffineTransformTranslate(transform, newSize.height, 0);
             transform = CGAffineTransformScale(transform, -1, 1);
+            break;
+            
+        case UIImageOrientationUp:
+        case UIImageOrientationDown:
+        case UIImageOrientationLeft:
+        case UIImageOrientationRight:
             break;
     }
     
