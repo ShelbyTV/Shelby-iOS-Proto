ShelbyTV/Shelby-iOS-Proto

commit 86441ffc3525b699984ca24065eda29d0b393e23

STVShareView now appears inside VideoPlayer rather than NavigationViewController. Closes GH-77.

Shelby/NavigationViewController.m
@@ -140,13 +140,17 @@
 
 #pragma mark - Layout
 
+- (CGRect)centerFrame:(CGRect)frame inFrame:(CGRect)parent {
+    frame.origin.x = (parent.size.width / 2) - (frame.size.width / 2);
+    frame.origin.y = (parent.size.height / 2) - (frame.size.height / 2);
+
+    return frame;
+}
+
 - (CGRect)centerFrame:(CGRect)frame
 {
-    frame.origin.x = (self.view.bounds.size.width / 2) - (frame.size.width / 2);
-    frame.origin.y = (self.view.bounds.size.height / 2) - (frame.size.height / 2);
-
-    //frame.origin.y = 0;
-    return frame;
+    return [self centerFrame:frame
+                     inFrame:self.view.bounds];
 }
 
 #pragma mark - Logout Functionality
@@ -274,8 +278,10 @@
         //CGRect frame = shareView.frame;
         //frame.origin.x = (self.view.bounds.size.width / 2) - (shareView.bounds.size.width / 2);
         //frame.origin.y = (self.view.bounds.size.height / 2) - (shareView.bounds.size.height / 2);
-        shareView.frame = [self centerFrame: shareView.frame];
-        [self.view addSubview: shareView];
+        shareView.frame = [self centerFrame: shareView.frame inFrame: _videoPlayer.bounds];
+
+        //[self.view addSubview: shareView];
+        [_videoPlayer addSubview: shareView];
 
         self.shareView = shareView;
 

commit 7c2f2d9a493a614a2435d8302ac4b9af994c02b0

Corrected a layout bug introduced in 86441ffc3525b699984ca24065eda29d0b393e23.

Shelby/NavigationViewController.m
@@ -417,7 +417,7 @@
     [UIView setAnimationDuration: animationDuration];
 
     // move the shareView
-    self.shareView.frame = [self centerFrame: self.shareView.frame];
+    self.shareView.frame = [self centerFrame: self.shareView.frame inFrame: _videoPlayer.bounds];
 
     [UIView commitAnimations];
 