f5dce1576eae39a743ddcef02efdf55acaac4ff7
Mark Johnson <markjohnson@gmail.com>
2011-10-18 14:57:08 -0700

    Remove STV prefix from ZeroEdgeTextView

---
+++ Shelby/ZeroEdgeTextView.h
@@ -0,0 +1,20 @@
+//
+//  ZeroEdgeTextView.h
+//  Shelby
+//
+//  Created by David Kay on 10/3/11.
+//  Copyright 2011 Gargoyle Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+/**
+ * TextView subclass. Takes advantage of the fact that UITextView is a subclass
+ * of UIScrollView in order to eliminate unwanted scrolling up/down when
+ * entering text.
+ *
+ * http://stackoverflow.com/questions/1178010/how-to-stop-uitextview-from-scrolling-up-when-entering-it
+ */
+@interface ZeroEdgeTextView : UITextView
+
+@end
---
+++ Shelby/ZeroEdgeTextView.m
@@ -0,0 +1,15 @@
+//
+//  ZeroEdgeTextView.m
+//  Shelby
+//
+//  Created by David Kay on 10/3/11.
+//  Copyright 2011 Gargoyle Software. All rights reserved.
+//
+
+#import "ZeroEdgeTextView.h"
+
+@implementation ZeroEdgeTextView
+
+- (UIEdgeInsets) contentInset { return UIEdgeInsetsZero; }
+
+@end
